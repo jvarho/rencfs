9ee76afffcfc631770895057f807ed8c1c2bad87
Jan Varho <jan@varho.org>
2016-10-16 16:37:36 +0300

    Rename to RencFS

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-python enc.py test1 test2 'helloworld' &
-python enc.py test2 test3 'helloworld' -d &
+python rencfs.py test1 test2 'helloworld' &
+python rencfs.py test2 test3 'helloworld' -d &
 sleep 1
 python testreader.py
 kill %2
